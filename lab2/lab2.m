7ed172f1c76bd0b0a67bbdca530a6b39c547b4f1
Konstantin Novikov <vokivon@Konstantins-MacBook-Air.local>
2021-10-21 12:38:52 +0300

    lab2

---
+++ lab2/lab2.m
@@ -0,0 +1,29 @@
+clear all
+close all
+N=200;
+K=400;
+mu=4; %среднее по ансамблю
+sigma=8;
+xi = normrnd(mu,sigma,[N,K]); % генерация матрицы с К реализациями случайного процесса
+
+%Строим среднее по ансамблю
+figure
+xi_average=zeros(N,1);
+for i=1:N;
+    for j=1:K;
+        xi_average(i) = xi_average(i) + xi(i,j)/K;
+    end
+end
+plot([1:N],xi_average)
+
+hold on
+
+% Строим среднее по времени
+xi_average_t=zeros(K,1);
+for i=1:K;
+    for j=1:N;
+        xi_average_t(i) = xi_average_t(i) + xi(j,i)/N;
+    end
+end
+plot([1:K],xi_average_t)
+                       
