3ba2db414fd2470f7b0d93df219022ffdf87ac23
Konstantin Novikov <vokivon@192.168.1.13>
2021-12-04 14:14:10 +0300

    markov_chain

--- lab3/markov_chain.m
+++ lab3/markov_chain.m
@@ -7,7 +7,6 @@ p=[2,5,3,4; 8,5,4,2; 5,4,3,2; 1,5,4,4];
 %генерируем цепь маркова
 mc = dtmc(p);
 mc.StateNames = ["Healthy", "Unwell", "Sick", "Very Sick"];
-mc.P
 
 %проверяем что сумма всех вероятностей = 1
 for i=1:length(mc.P(:,1))
@@ -23,21 +22,63 @@ graphplot(mc, "ColorEdges", true)
 
 P_cum=cumsum(mc.P, 2)
 
+%{
+----Генерируем поведение цепи Маркова----
+%}
 
-z_t=zeros(1,200)
-z_t(1,1)=1
-for i=1:200;
-    r=rand
-    for j=1:mc.NumStates
-        if r>P_cum(z_t(1,i),j)
-            z_t(1,i+1)=j
-        else
-            z_t(1,i+1)=z_t(1,i)
-        end
-    end
-
-
-end
+K=200
+z_t_200=markov_chain_bh(mc, P_cum, 200)
+z_t_1000=markov_chain_bh(mc, P_cum, 1000)
+z_t_10000=markov_chain_bh(mc, P_cum, 10000)
 
 figure
-plot(z_t)
+subplot(3,1,1)
+plot(z_t_200)
+subplot(3,1,2)
+plot(z_t_1000)
+subplot(3,1,3)
+plot(z_t_10000)
+
+P_obs_200=markov_obs(z_t_200)
+P_obs_1000=markov_obs(z_t_1000)
+P_obs_10000=markov_obs(z_t_10000)
+
+mc_obs_200=dtmc(P_obs_200)
+mc_obs_1000=dtmc(P_obs_1000)
+mc_obs_10000=dtmc(P_obs_10000)
+
+figure
+subplot(3,1,1)
+graphplot(mc_obs_200, "ColorEdges", true)
+
+subplot(3,1,2)
+graphplot(mc_obs_1000, "ColorEdges", true)
+
+subplot(3,1,3)
+graphplot(mc_obs_10000, "ColorEdges", true)
+
+
+function P_obs=markov_obs(z_t)
+P_obs=zeros(4,4);
+for i=1:length(z_t)-1;
+    P_obs(z_t(i),z_t(i+1))=P_obs(z_t(i),z_t(i+1))+1;
+end
+P_obs=P_obs./length(z_t);        %нормируем
+end
+
+function z_t=markov_chain_bh(mc, P_cum, K)
+z_t=zeros(1,K);
+z_t(1,1)=1;
+for i=1:K-1;
+    r=randi(100)/100;
+    for j=1:mc.NumStates;
+        if r>=P_cum(z_t(1,i),j);
+            z_t(1,i+1)=j;
+        elseif z_t(1,i+1)==0;
+            z_t(1,i+1)=z_t(1,i);
+        end
+    end
+end
+end
+
+
